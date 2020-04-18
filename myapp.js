fc717969f47e30709a7e1f2d515d93b9cb12ae8b
jatin mehrotra <jatinjmehrotra@gmail.com>
2020-04-18 15:14:44 +0530

    alert box added

--- myapp.js
+++ myapp.js
@@ -1,27 +1,27 @@
 
-let element=document.getElementById('set');
-let divele=document.getElementById('modify');
+let element = document.getElementById('set');
+let divele = document.getElementById('modify');
+let optionsarray = document.getElementsByName('cssProperty');
+let alertobj={};
 
-let optionsarray=document.getElementsByName('cssProperty');
-element.addEventListener('click',userClickedSet);
-
-
-function userClickedSet(){
-
-  alert('you clicked');
+element.addEventListener('click', userClickedSet);
 
 
-console.log(optionsarray);
-
-for(let i=0; i<optionsarray.length;i++){
-
-  let value=optionsarray[i].value;
-  let property=optionsarray[i].getAttribute('id');
-  console.log(property,value);
-  let divele=document.getElementById('modify');
-  divele.style[property]=value;
-}
+function userClickedSet() {
 
 
+  for (let i = 0; i < optionsarray.length; i++) {
+
+    let value = optionsarray[i].value;
+    let property = optionsarray[i].getAttribute('id');
+    console.log(property, value);
+
+    alertobj[property]=value;
+
+    divele.style[property] = value;
+
+  }
+
+alert("You selected background: "+alertobj.background+",width: "+alertobj.width+ ",height: "+alertobj.height);
 
 }
